c00d7caf46354e1bb11e5ec8ca6cad84ecd667eb
Mateus Ribeiro <ribeiromateus0042@gmail.com>
2024-02-09 17:49:30 -0300

    feat: adiciona modal
    utilizando a biblioteca dialog do radix criei a base do modal

--- src/app.tsx
+++ src/app.tsx
@@ -25,7 +25,10 @@ export function App() {
           </p>
         </div>
 
-        <NoteCard />
+        <NoteCard note={{
+          date: new Date(),
+          content: "Hello World"
+        }}/>
       </div>
     </div>
   )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,27 +1,53 @@
-export function NoteCard(){
-  return (
-    <button className="
-      text-left 
-      rounded-md 
-      p-5 space-y-3 
-      overflow-hidden 
-      relative 
-      hover:ring-2 
-      focus-visible:ring-2 
-      outline-none  
-      hover:ring-slate-600 
-      bg-slate-800 
-      focus-visible:ring-lime-400
-    ">
-      <span className="text-sm font-medium text-slate-300">Há 2 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, nulla nihil cupiditate sapiente excepturi eveniet facilis eaque voluptas odio nemo recusandae quod eum veniam ratione, fugit cumque. Ab, deserunt incidunt.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique nulla unde eos id? Enim, voluptatum consequuntur. Pariatur quas voluptate autem modi sit vel eos nam, quae sequi quibusdam qui incidunt!
-      </p>
+import * as Dialog from "@radix-ui/react-dialog";
 
-      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t
-        from-black/60 to-black/0 pointer-events-none" 
-      />
-    </button>
+interface NoteCardProps {
+  note: {
+    date: Date
+    content: string
+  }
+}
+
+export function NoteCard({ note }: NoteCardProps){
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="
+        text-left 
+        rounded-md 
+        p-5 
+        gap-3 
+        overflow-hidden 
+        relative 
+        hover:ring-2 
+        focus-visible:ring-2 
+        outline-none  
+        flex
+        flex-col
+        hover:ring-slate-600 
+        bg-slate-800 
+        focus-visible:ring-lime-400
+      ">
+        <span className="text-sm font-medium text-slate-300">{note.date.toISOString()}</span>
+        <p className="text-sm leading-6 text-slate-400">
+          {note.content}
+        </p>
+
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t
+          from-black/60 to-black/0 pointer-events-none" 
+        />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="fixed z-10 left-1/2 top-1/2 -translate-x-1/2 
+        -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
+          <div className="flex flex-1 flex-col gap-3 p-5">
+          <span className="text-sm font-medium text-slate-300">{note.date.toISOString()}</span>
+          <p className="text-sm leading-6 text-slate-400">
+            {note.content}
+          </p>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
